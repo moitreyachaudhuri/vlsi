71e8b1af4f39669548d10776496669385349ecd3
moitreyachaudhuri <111184171+moitreyachaudhuri@users.noreply.github.com>
2024-09-04 19:20:24 +0530

    Update seq_det.v

--- seq_det.v
+++ seq_det.v
@@ -1,26 +1,3 @@
-/********************************************************************************************
-Copyright 2019 - Maven Silicon Softech Pvt Ltd. 
-
-All Rights Reserved.
-
-This source code is an unpublished work belongs to Maven Silicon Softech Pvt Ltd.
-
-It is not to be shared with or used by any third parties who have not enrolled for our paid training 
-
-courses or received any written authorization from Maven Silicon.
-
-
-Webpage     :      www.maven-silicon.com
-
-Filename    :	   seq_det.v   
-
-Description :      Sequence detector detecting "101"
-
-
-Author Name :      Susmita
-
-Version     :      1.0
-*********************************************************************************************/
 
 module seq_det(seq_in,
 	       clock,
